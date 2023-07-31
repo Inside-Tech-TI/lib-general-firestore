2fe0ad9f49fc1dfe92cd186d6401f3cb49b1a4f9
Renato <reytuty@gmail.com>
2023-07-31 16:36:28 -0300

    chore: update test

--- tests/find.test.ts
+++ tests/find.test.ts
@@ -11,19 +11,123 @@ const collection = (col: string) => {
 const collectionTestName: string = "test_collection";
 const firestore = collection(collectionTestName);
 const itens = [
-  { id: "a", a: 2, b: 1, name: "test1", type: "a" },
-  { id: "b", a: 4, b: 2, name: "test2", type: "a" },
-  { id: "c", a: 6, b: 3, name: "test3", type: "b" },
-  { id: "d", a: 8, b: 4, name: "test4", type: "b" },
-  { id: "e", a: 10, b: 5, name: "test5", type: "b" },
-  { id: "f", a: 11, b: 6, name: "test5", type: "b" },
-  { id: "g", a: 10, b: 7, name: "test5", type: "b" },
-  { id: "h", a: 10, b: 8, name: "test5", type: "b" },
-  { id: "i", a: 10, b: 9, name: "test5", type: "b" },
-  { id: "j", a: 10, b: 10, name: "test5", type: "b" },
-  { id: "k", a: 10, b: 11, name: "test5", type: "b" },
-  { id: "l", a: 10, b: 12, name: "test5", type: "b" },
-  { id: "m", a: 10, b: 13, name: "test5", type: "c" },
+  {
+    id: "a",
+    a: 2,
+    b: 1,
+    name: "test1",
+    hasAccess: true,
+    type: "a",
+    itens: ["a", "b", "c"],
+  },
+  {
+    id: "b",
+    a: 4,
+    b: 2,
+    name: "test2",
+    hasAccess: true,
+    type: "a",
+    itens: ["a", "b"],
+  },
+  {
+    id: "c",
+    a: 6,
+    b: 3,
+    name: "test3",
+    hasAccess: true,
+    type: "b",
+    itens: ["a", "b"],
+  },
+  {
+    id: "d",
+    a: 8,
+    b: 4,
+    name: "test4",
+    hasAccess: true,
+    type: "b",
+    itens: ["a", "b"],
+  },
+  {
+    id: "e",
+    a: 10,
+    b: 5,
+    name: "test5",
+    hasAccess: true,
+    type: "b",
+    itens: ["a", "b"],
+  },
+  {
+    id: "f",
+    a: 11,
+    b: 6,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a", "b"],
+  },
+  {
+    id: "g",
+    a: 10,
+    b: 7,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a", "b"],
+  },
+  {
+    id: "h",
+    a: 10,
+    b: 8,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a", "b"],
+  },
+  {
+    id: "i",
+    a: 10,
+    b: 9,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a"],
+  },
+  {
+    id: "j",
+    a: 10,
+    b: 10,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a"],
+  },
+  {
+    id: "k",
+    a: 10,
+    b: 11,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a"],
+  },
+  {
+    id: "l",
+    a: 10,
+    b: 12,
+    name: "test5",
+    hasAccess: false,
+    type: "b",
+    itens: ["a"],
+  },
+  {
+    id: "m",
+    a: 10,
+    b: 13,
+    name: "test5",
+    hasAccess: false,
+    type: "c",
+    itens: ["a"],
+  },
 ];
 describe("Upsert and find", () => {
   test("Upsert itens", async () => {
@@ -45,6 +149,30 @@ describe("Upsert and find", () => {
     expect(result.total).toBeGreaterThan(2);
     expect(result.data.length).toBe(2);
   });
+  test("Find itens by array", async () => {
+    const result = await firestore.find<{
+      id: string;
+      a: number;
+      b: number;
+      name: string;
+      type: string;
+      itens: string[];
+    }>({ itens: { $all: ["c"] } }, undefined);
+    expect(result.length).toBe(1);
+    expect(result[0].id).toBe("a");
+  });
+  test("Find itens by array contains any itens", async () => {
+    const result = await firestore.find<{
+      id: string;
+      a: number;
+      b: number;
+      name: string;
+      type: string;
+      itens: string[];
+    }>({ itens: { $all: ["b", "c"] }, hasAccess: true }, undefined);
+    expect(result.length).toBeGreaterThan(2);
+  });
+
   test("Find itens with filter and total", async () => {
     const result = await firestore.findWithTotal<{
       id: string;
